452e77be12e1c1450e44289569e021cd766b3233
sumin <sumin00716@naver.com>
2021-08-08 15:42:53 +0900

    donghakgaemi app, third commit

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class stockModel(models.Model):
+    class Meta:
+        db_table = "stock"
+
+    stock_id = models.IntegerField(null=False, blank=False)
+    stock_name = models.CharField(max_length=20, null=False, blank=False)
+    stock_code = models.IntegerField(null=False, blank=False)
+    stock_category = models.CharField(max_length=20, null=False, blank=False)
+    stock_market_capitalization = models.IntegerField(null=False, blank=False)
+    stock_foundation_year = models.IntegerField(null=False, blank=False)
+    stock_listing_date = models.CharField(max_length=20, null=False, blank=False)
