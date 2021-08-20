edae3d604b3fd18bc58c74389d387e2512b33704
sumin <sumin00716@naver.com>
2021-08-20 13:29:28 +0900

    Modify urls file

--- user/models.py
+++ user/models.py
@@ -1,29 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, AbstractBaseUser, BaseUserManager
 
-class UserModel(AbstractUser):
+class UserModel(models.Model):
     class Meta:
         db_table = "my_user"
 
-    username = None
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    email = models.CharField(max_length=100, null=False, blank=False, unique=True)
-    name = models.CharField(max_length=30, default='', null=False, blank=False)
+    user_id = models.IntegerField(null=False, blank=False)
+    email = models.CharField(max_length=50, null=False, blank=False)
+    pwd = models.CharField(max_length=50, null=False, blank=False)
+    username = models.CharField(max_length=30, null=False, blank=False)
     phone = models.CharField(max_length=30, null=False, blank=False)
     birthdate = models.DateField(null=False, blank=False)
     gender = models.CharField(max_length=10, null=False, blank=False)
-
-    def create_superuser(self, username, password):
-        if not username:
-            raise ValueError("User must have a name")
-        if not password:
-            raise ValueError("User must have a password")
-
-        user = self.model(
-            username = self.normalize_username(username)
-        )
-        user.username= username
-        user.set_password(password)
-        return user
