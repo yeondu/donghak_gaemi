d2a53a572f1f8721f4f22088e853c716ed19588d
yeonsu <ahh0123@naver.com>
2021-09-05 18:54:44 +0900

    모델 수정, view 수정

--- stock/models.py
+++ stock/models.py
@@ -4,7 +4,7 @@ class stockModel(models.Model):
         db_table = "stock"
 
     stock_name = models.CharField(max_length=20, null=False, blank=False)
-    stock_code = models.CharField(max_length = 10, null=False, blank=False)
+    stock_code = models.CharField(primary_key = True, max_length = 10, null=False, blank=False)
     stock_category = models.CharField(max_length=20, null=False, blank=False)
     stock_foundation_year = models.DateField(null=False, blank=False)
     stock_listing_date = models.DateField(null=False, blank=False)
--- stock/views.py
+++ stock/views.py
@@ -5,14 +5,25 @@ from django.core.paginator import Paginator
 from django.views.generic import ListView
 from django.views.generic import View
 from time import mktime, strptime
-from .models import stockModel, priceModel
+from .models import stockModel, priceModel, predictModel, accuracyModel
 
 # Create your views here.
 
 # 종목 리스트
 def home(request):
-    all_list = priceModel.objects.all()
-    return render(request, 'stock/list.html', {'list':all_list})
+    stock = stockModel.objects.all()
+    price = priceModel.objects.all()
+    pr = predictModel.objects.all()
+    acc = accuracyModel.objects.all()
+
+    list = {'stock_name':stock.stock_name,
+            'stock_code':stock.stock_code,
+            'close':price.close,
+            'predict':pr.result,
+            'accuracy':acc.result
+            }
+
+    return render(request, 'stock/list.html', list)
 
 # 종목 상세페이지(뉴스)
 def detail_stock_news(request):
