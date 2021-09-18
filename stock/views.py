f8706387058771edc063180315dbb2fc2ab0e480
yeonsu <ahh0123@naver.com>
2021-09-18 23:08:52 +0900

    리스트 페이지 수정

--- stock/views.py
+++ stock/views.py
@@ -1,33 +1,26 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
-from .models import stockModel, priceModel
 from django.core.paginator import Paginator
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 from django.views.generic import View
 from time import mktime, strptime
-from .models import stockModel, priceModel, predictModel, accuracyModel
-
+from stock.models import stockModel, priceModel, predictModel, accuracyModel
+import pandas as pd
 # Create your views here.
 
 # 종목 리스트
+
 def home(request):
-    stock = stockModel.objects.all()
-    price = priceModel.objects.all()
-    pr = predictModel.objects.all()
-    acc = accuracyModel.objects.all()
+    name = stockModel.objects.values('stock_name')
+    return render(request, 'stock/list.html', {'name':name})
 
-    list = {'stock_name':stock.stock_name,
-            'stock_code':stock.stock_code,
-            'close':price.close,
-            'predict':pr.result,
-            'accuracy':acc.result
-            }
-
-    return render(request, 'stock/list.html', list)
 
 # 종목 상세페이지(뉴스)
-def detail_stock_news(request):
-    return render(request, 'stock/detail.html')
+def detail_stock_news(request, id):
+    price = priceModel.objects.get(stock_id = id)
+    stock = price.stock_code
+    #stock = stockModel.objects.filter(stock_code = price['stock_code_id']).values()
+    return render(request, 'stock/detail.html', {'price':price, 'stock':stock})
 
 
 
