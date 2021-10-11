9393352dad282b11776aaccc73cc0899b6b12b06
Ji Hyun <wus570708@naver.com>
2021-10-12 00:49:48 +0900

    prod database 엔진 이전 것으로 변경

--- Donghakgaemi/settings/prod.py
+++ Donghakgaemi/settings/prod.py
@@ -2,13 +2,3 @@ from .base import *
 
 ALLOWED_HOSTS = ['15.165.31.148']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'Donghakgaemi',
-        'USER': 'donghakgaemi',
-        'PASSWORD': 'donghakmysql1',
-        'HOST': '15.165.31.148',
-        'PORT': '3306',
-    }
-}
